44ff8b7c5902fb3484ba2fa2390f8bfae05fb5d5
sasidharan10 <ksasidharan98@gmail.com>
2023-07-13 20:03:14 +0530

    new utility added

--- static/js/app.js
+++ static/js/app.js
@@ -24,129 +24,136 @@ map.addControl(
 );
 
 map.addControl(new mapboxgl.NavigationControl());
-
-const url = "/data";
-// const url = "https://api.covid19api.com/summary";
-
 // map.dragRotate.disable();
 // map.touchZoomRotate.disableRotation();
 map.scrollZoom.disable();
 
-let jsonString = JSON.stringify(url);
-let jsonData = JSON.parse(jsonString);
 
-fetch(jsonData)
-    .then(Response =>
-        Response.json())
+let jsonString = JSON.stringify("/data");
+const jsonData = JSON.parse(jsonString);   // to get local json data
+
+const url = "https://api.covid19api.com/summary";
+// const url = "https://mycovidapi-sj9z.onrender.com"; // custom API
+
+fetch(url)
+    .then((Response) => {
+        console.log("API is working fine");
+        return Response.json()
+    }).catch((err) => {
+        console.log("Covid19 API is down, Hence, data will be fetched from local json file");
+        return fetch(jsonData).then(Response =>
+            Response.json());
+    })
     .then(res => {
-        console.log("res executing");
-        // console.log(res.Countries);  // checking the data
+        console.log("Populating data");
+        populateData(res);
+    }).catch(err => {
+        console.log(err);
+        console.log("Error Occured");
+    });
 
-        var worldCases = document.getElementById("world-cases-count");
-        var worldRecovered = document.getElementById("world-recovered-count");
-        var worldDeaths = document.getElementById("world-deaths-count");
+function populateData(res) {
+    var worldCases = document.getElementById("world-cases-count");
+    var worldRecovered = document.getElementById("world-recovered-count");
+    var worldDeaths = document.getElementById("world-deaths-count");
 
-        var worldCasesCount = `<p>${res.Global.TotalConfirmed.toLocaleString()}</p>`;
-        var worldRecoveredCount = `<p>${res.Global.TotalRecovered.toLocaleString()}</p>`;
-        var worldDeathsCount = `<p>${res.Global.TotalDeaths.toLocaleString()}</p>`;
+    var worldCasesCount = `<p>${res.Global.TotalConfirmed.toLocaleString()}</p>`;
+    var worldRecoveredCount = `<p>${res.Global.TotalRecovered.toLocaleString()}</p>`;
+    var worldDeathsCount = `<p>${res.Global.TotalDeaths.toLocaleString()}</p>`;
 
-        worldCases.innerHTML = worldCasesCount;
-        worldRecovered.innerHTML = worldRecoveredCount;
-        worldDeaths.innerHTML = worldDeathsCount;
+    worldCases.innerHTML = worldCasesCount;
+    worldRecovered.innerHTML = worldRecoveredCount;
+    worldDeaths.innerHTML = worldDeathsCount;
 
-        let i = 1;
+    let i = 1;
 
-        var table = document.getElementById("country-body");
+    var table = document.getElementById("country-body");
 
-        res.Countries.forEach(elm => {
+    res.Countries.forEach(elm => {
 
-            var details =
-                `<tr>
+        var details =
+            `<tr>
                     <td>${i}</td>
                     <td>${elm.Slug.toLocaleString()}</td>
                     <td id="table-cases">${elm.TotalConfirmed.toLocaleString()}</td>
                     <td id="table-recovered">${elm.TotalRecovered.toLocaleString()}</td>
                     <td id="table-deaths">${elm.TotalDeaths.toLocaleString()}</td>
                 </tr>`;
-            i++;
+        i++;
 
-            table.innerHTML += details;
+        table.innerHTML += details;
 
-            let country = elm.Slug;
-            let cases = elm.TotalConfirmed;
-            let recovered = elm.TotalRecovered.toLocaleString();
-            let death = elm.TotalDeaths.toLocaleString();
-            let colors;
+        let country = elm.Slug;
+        let cases = elm.TotalConfirmed;
+        let recovered = elm.TotalRecovered.toLocaleString();
+        let death = elm.TotalDeaths.toLocaleString();
+        let colors;
 
-            if (cases >= 1000000) {
-                colors = "rgb(255, 0, 0)";
-            }
-            else if (cases > 300000 && cases < 999999) {
-                colors = "rgb(255, 102, 0)";
-            }
-            else if (cases > 100000 && cases < 299999) {
-                colors = "rgb(255, 255, 0)";
-            }
-            else if (cases > 20000 && cases < 99999) {
-                colors = "rgb(0, 204, 0)";
-            }
-            else {
-                colors = "rgb(0, 255, 255)";
-            }
+        if (cases >= 1000000) {
+            colors = "rgb(255, 0, 0)";
+        }
+        else if (cases > 300000 && cases < 999999) {
+            colors = "rgb(255, 102, 0)";
+        }
+        else if (cases > 100000 && cases < 299999) {
+            colors = "rgb(255, 255, 0)";
+        }
+        else if (cases > 20000 && cases < 99999) {
+            colors = "rgb(0, 204, 0)";
+        }
+        else {
+            colors = "rgb(0, 255, 255)";
+        }
 
-            let fcases = cases.toLocaleString(); // in million format
+        let fcases = cases.toLocaleString(); // in million format
 
-            var popup = new mapboxgl.Popup(
-                {
-                    offset: [0, 0],
-                    closeButton: false
+        var popup = new mapboxgl.Popup(
+            {
+                offset: [0, 0],
+                closeButton: false
+            }
+        ).setHTML(
+            '<h3>' + country + '</h3><p id="infected">Infected &nbsp;&nbsp;&nbsp;&nbsp;&nbsp :&nbsp '
+            + fcases + '</p><p id="recovered">Recovered &nbsp:&nbsp ' + recovered +
+            '</p><p id="death">Deaths &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp : &nbsp'
+            + death + '</p>'
+        )
+
+        var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+        mapboxClient.geocoding
+            .forwardGeocode({
+                query: `${country}`,
+                autocomplete: false,
+                limit: 1
+            })
+            .send()
+            .then(function (response) {
+                if (
+                    response &&
+                    response.body &&
+                    response.body.features &&
+                    response.body.features.length
+                ) {
+                    var feature = response.body.features[0];
+                    // console.log(response);
+
+                    // Create a marker and add it to the map.
+                    let marker = new mapboxgl.Marker({
+                        draggable: false,
+                        color: colors,
+                        scale: 0.6  // resize
+                    }).setLngLat(feature.center).addTo(map);
+
+                    var element = marker.getElement();
+                    // element.id = 'marker';
+                    // hover event listener
+                    element.addEventListener('mouseenter', () => popup.addTo(map));
+                    element.addEventListener('mouseleave', () => popup.remove());
+
+                    // add popup to marker
+                    marker.setPopup(popup);
+
                 }
-            ).setHTML(
-                '<h3>' + country + '</h3><p id="infected">Infected &nbsp;&nbsp;&nbsp;&nbsp;&nbsp :&nbsp '
-                + fcases + '</p><p id="recovered">Recovered &nbsp:&nbsp ' + recovered +
-                '</p><p id="death">Deaths &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp : &nbsp'
-                + death + '</p>'
-            )
-
-            var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
-            mapboxClient.geocoding
-                .forwardGeocode({
-                    query: `${country}`,
-                    autocomplete: false,
-                    limit: 1
-                })
-                .send()
-                .then(function (response) {
-                    if (
-                        response &&
-                        response.body &&
-                        response.body.features &&
-                        response.body.features.length
-                    ) {
-                        var feature = response.body.features[0];
-                        // console.log(response);
-
-                        // Create a marker and add it to the map.
-                        let marker = new mapboxgl.Marker({
-                            draggable: false,
-                            color: colors,
-                            scale: 0.6  // resize
-                        }).setLngLat(feature.center).addTo(map);
-
-                        var element = marker.getElement();
-                        // element.id = 'marker';
-                        // hover event listener
-                        element.addEventListener('mouseenter', () => popup.addTo(map));
-                        element.addEventListener('mouseleave', () => popup.remove());
-
-                        // add popup to marker
-                        marker.setPopup(popup);
-
-                    }
-                });
-        });
-    }).catch(err => {
-        console.log(err);
-        console.log("Error Occured");
+            });
     });
-
+}
