95c75ee87dfe83ba8143db9224504241025d8027
sasidharan10 <ksasidharan98@gmail.com>
2023-07-23 13:46:13 +0530

    CHanged to new API

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const host = "127.0.0.1";
 const port = process.env.PORT || 5000;
 
+const RapidApiKey = process.env.RAPIDAPIKEY;
+const RapidApiHost = process.env.RAPIDAPIHOST;
+
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'html');
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+  res.render('index');
 });
 
 app.get('/data', (req, res) => {
-    res.sendFile(__dirname + '/static/json/covid19_data.json');
+  res.sendFile(__dirname + '/static/json/newCovid.json');
+});
+
+app.get('/api', (req, res) => {
+  const url = "https://covid-19-tracking.p.rapidapi.com/v1";
+  const options = {
+    method: 'GET',
+
+    headers: {
+      'X-RapidAPI-Key': RapidApiKey,
+      'X-RapidAPI-Host': RapidApiHost
+    }
+  };
+  axios.get(url, options)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+
 });
 
 app.listen(port, () => {
-    console.log(`The application started successfully at : http://${host}:${port}`);
+  console.log(`The application started successfully at : http://${host}:${port}`);
 });
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2FzaWRoYXJhbjEwIiwiYSI6ImNrcDhhdGtlYzA4Z3EydXA3OXc0NHl3N3QifQ.hhlLewMbrP5XZYGfGhhA0w';
+mapboxgl.accessToken = "pk.eyJ1Ijoic2FzaWRoYXJhbjEwIiwiYSI6ImNrcDhhdGtlYzA4Z3EydXA3OXc0NHl3N3QifQ.hhlLewMbrP5XZYGfGhhA0w";
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -32,10 +32,11 @@ map.scrollZoom.disable();
 let jsonString = JSON.stringify("/data");
 const jsonData = JSON.parse(jsonString);   // to get local json data
 
-const url = "https://api.covid19api.com/summary";
+// const url = "https://covid-19-tracking.p.rapidapi.com/v1";
+// const url = "https://api.covid19api.com/summary";
 // const url = "https://mycovidapi-sj9z.onrender.com"; // custom API
 
-fetch(url)
+fetch("/api")
     .then((Response) => {
         console.log("API is working fine");
         return Response.json()
@@ -53,15 +54,13 @@ fetch(url)
     });
 
 function populateData(res) {
-    var worldCases = document.getElementById("world-cases-count");
-    var worldRecovered = document.getElementById("world-recovered-count");
-    var worldDeaths = document.getElementById("world-deaths-count");
+    let worldCases = document.getElementById("world-cases-count");
+    let worldRecovered = document.getElementById("world-recovered-count");
+    let worldDeaths = document.getElementById("world-deaths-count");
 
-    const newGlobalTotalRecovered = getRecovered(res.Global.TotalConfirmed - res.Global.TotalDeaths, 3);
-
-    var worldCasesCount = `<p>${res.Global.TotalConfirmed.toLocaleString()}</p>`;
-    var worldRecoveredCount = `<p>${newGlobalTotalRecovered.toLocaleString()}</p>`;
-    var worldDeathsCount = `<p>${res.Global.TotalDeaths.toLocaleString()}</p>`;
+    let worldCasesCount = `<p>${res[0]["Total Cases_text"]}</p>`;
+    let worldRecoveredCount = `<p>${res[0]["Total Recovered_text"]}</p>`;
+    let worldDeathsCount = `<p>${res[0]["Total Deaths_text"]}</p>`;
 
     worldCases.innerHTML = worldCasesCount;
     worldRecovered.innerHTML = worldRecoveredCount;
@@ -69,60 +68,74 @@ function populateData(res) {
 
     let i = 1;
 
-    var table = document.getElementById("country-body");
+    let table = document.getElementById("country-body");
 
-    res.Countries.forEach(elm => {
+    res.filter((a, b) => {
+        return b > 0 && b < 232;
+    }).forEach(elm => {
 
-        const newTotalRecovered = getRecovered(elm.TotalConfirmed - elm.TotalDeaths, 0.1);
-        var details =
+        const country = elm.Country_text;
+        let totalCases, totalRecovered, totalDeath;
+        if (elm["Total Cases_text"] !== "")
+            totalCases = elm["Total Cases_text"];
+        else
+            totalCases = "N/A";
+
+        if (elm["Total Recovered_text"] !== "")
+            totalRecovered = elm["Total Recovered_text"];
+        else
+            totalRecovered = "N/A";
+
+        if (elm["Total Deaths_text"] !== "")
+            totalDeath = elm["Total Deaths_text"];
+        else
+            totalDeath = "N/A";
+
+        const totalCasesInteger = parseInt(totalCases.replace(/,/g, ''), 10);
+
+        let details =
             `<tr>
-                    <td>${i}</td>
-                    <td>${elm.Slug.toLocaleString()}</td>
-                    <td id="table-cases">${elm.TotalConfirmed.toLocaleString()}</td>
-                    <td id="table-recovered">${newTotalRecovered.toLocaleString()}</td>
-                    <td id="table-deaths">${elm.TotalDeaths.toLocaleString()}</td>
-                </tr>`;
+                        <td>${i}</td>
+                        <td>${country}</td>
+                        <td id="table-cases">${totalCases}</td>
+                        <td id="table-recovered">${totalRecovered}</td>
+                        <td id="table-deaths">${totalDeath}</td>
+                    </tr>`;
         i++;
 
         table.innerHTML += details;
 
-        let country = elm.Slug;
-        let cases = elm.TotalConfirmed;
-        let recovered = newTotalRecovered.toLocaleString();
-        let death = elm.TotalDeaths.toLocaleString();
         let colors;
 
-        if (cases >= 1000000) {
+        if (totalCasesInteger >= 1000000) {
             colors = "rgb(255, 0, 0)";
         }
-        else if (cases > 300000 && cases < 999999) {
+        else if (totalCasesInteger > 300000 && totalCasesInteger < 999999) {
             colors = "rgb(255, 102, 0)";
         }
-        else if (cases > 100000 && cases < 299999) {
+        else if (totalCasesInteger > 100000 && totalCasesInteger < 299999) {
             colors = "rgb(255, 255, 0)";
         }
-        else if (cases > 20000 && cases < 99999) {
+        else if (totalCasesInteger > 20000 && totalCasesInteger < 99999) {
             colors = "rgb(0, 204, 0)";
         }
         else {
             colors = "rgb(0, 255, 255)";
         }
 
-        let fcases = cases.toLocaleString(); // in million format
-
-        var popup = new mapboxgl.Popup(
+        let popup = new mapboxgl.Popup(
             {
                 offset: [0, 0],
                 closeButton: false
             }
         ).setHTML(
             '<h3>' + country + '</h3><p id="infected">Infected &nbsp;&nbsp;&nbsp;&nbsp;&nbsp :&nbsp '
-            + fcases + '</p><p id="recovered">Recovered &nbsp:&nbsp ' + recovered +
+            + totalCases + '</p><p id="recovered">Recovered &nbsp:&nbsp ' + totalRecovered +
             '</p><p id="death">Deaths &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp : &nbsp'
-            + death + '</p>'
+            + totalDeath + '</p>'
         )
 
-        var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+        let mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
         mapboxClient.geocoding
             .forwardGeocode({
                 query: `${country}`,
@@ -137,7 +150,7 @@ function populateData(res) {
                     response.body.features &&
                     response.body.features.length
                 ) {
-                    var feature = response.body.features[0];
+                    let feature = response.body.features[0];
                     // console.log(response);
 
                     // Create a marker and add it to the map.
@@ -147,7 +160,7 @@ function populateData(res) {
                         scale: 0.6  // resize
                     }).setLngLat(feature.center).addTo(map);
 
-                    var element = marker.getElement();
+                    let element = marker.getElement();
                     // element.id = 'marker';
                     // hover event listener
                     element.addEventListener('mouseenter', () => popup.addTo(map));
@@ -159,10 +172,4 @@ function populateData(res) {
                 }
             });
     });
-}
-
-function getRecovered(a, p) {
-    let temp = Math.round((a * p) / 100);
-    let ans = a - temp;
-    return ans;
 }
